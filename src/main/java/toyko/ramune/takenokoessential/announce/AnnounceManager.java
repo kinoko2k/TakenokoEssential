789333a1f61927da6b9d7a4af2fe4bbfb548ea05
RamuneRemonedo <99951138+RamuneRemonedo@users.noreply.github.com>
2022-06-26 19:43:53 +0900

    ふさわしくない記述の修正

--- src/main/java/toyko/ramune/takenokoessential/announce/AnnounceManager.java
+++ src/main/java/toyko/ramune/takenokoessential/announce/AnnounceManager.java
@@ -34,7 +34,7 @@ public class AnnounceManager {
         }, 12000, 12000);
     }
     
-        private void runServerRuleAnnounce(JavaPlugin plugin) {
+    private void runServerRuleAnnounce(JavaPlugin plugin) {
         Bukkit.getScheduler().runTaskTimerAsynchronously(plugin, () -> {
             Bukkit.getOnlinePlayers().forEach((player -> {
                 player.sendMessage(ChatColor.AQUA + "[サーバー] サーバーには、ルールが定めてあります！知らなかったでは済まされません！https://takenoko.ramune.tokyo/rule/");
