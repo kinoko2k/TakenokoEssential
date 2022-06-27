17a751ccc27c3d2cd1fab3ced305cf3ff10e6f1a
RamuneRemonedo <99951138+RamuneRemonedo@users.noreply.github.com>
2022-06-27 17:13:39 +0900

    CreativeModeを削除しました

--- src/main/java/toyko/ramune/takenokoessential/listener/PlayerJoinListener.java
+++ src/main/java/toyko/ramune/takenokoessential/listener/PlayerJoinListener.java
@@ -20,13 +20,6 @@ public class PlayerJoinListener implements Listener {
         MenuHandler.giveCompassIfNotExist(player);
         player.sendMessage(ChatColor.GREEN + "サーバーを移動したいときは");
         player.sendMessage(ChatColor.GREEN + "/menuでサーバーメニューを開けるよ!");
-        if (TakenokoEssential.getConfigFile().CREATIVE_SERVER_MODE_ENABLE) {
-            player.sendMessage(ChatColor.AQUA + "[クリエイティブサーバーでの遊び方]");
-            player.sendMessage(ChatColor.LIGHT_PURPLE + "1.自分の保護を作成してから建築をしよう! /menuから保護の設定ができるよ");
-            player.sendMessage(ChatColor.LIGHT_PURPLE + "2.自分の建築の場所を/sethomeを使って移動を楽にしよう! /sethomeで登録すると、遠くからでも移動ができるよ!");
-            player.sendMessage(ChatColor.LIGHT_PURPLE + "3.他の人とも建築が一緒にできるよ! 他のプレイヤーを自分の保護に追加しよう!");
-            player.sendMessage(ChatColor.LIGHT_PURPLE + "4.プレイ時間によって、保護できる範囲のブロック数が増えるよ!");
-        }
         if (!TakenokoEssential.getConfigFile().SPAWN_LOCATION_FORCE_TELEPORT) {
             return;
         }
